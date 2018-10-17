d3c42b7598e5fe671e8ceb9cd16bead9887863bf
MacbookPro <macbookpro@Alagedrew.local>
2018-10-17 11:30:32 +0700

    Initial Commit

---
+++ Murotal Player/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Murotal Player
+//
+//  Created by Zahid on 10/17/18.
+//  Copyright © 2018 Haqqani Project. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
